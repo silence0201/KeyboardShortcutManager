a4d898d84f3a32b462f57d92332f80f47386bd74
Silence <374619540@qq.com>
2017-02-19 15:07:05 +0800

    Update

--- KeyboardShortcutManager/SIKeyboardShortcutManager.h
+++ KeyboardShortcutManager/SIKeyboardShortcutManager.h
@@ -14,10 +14,24 @@ typedef void(^KeyBoardAction)();
 
 @interface SIKeyboardShortcutManager : NSObject
 
-@property (nonatomic,assign,getter=isEnabled) BOOL enabled ;
+@property (nonatomic,assign,getter=isEnabled) BOOL enabled ; /// 是否开启,默认是开启
 
+
+/**
+ 单例
+
+ @return Manager单例
+ */
 + (instancetype)sharedManager ;
 
+
+/**
+ 注册快捷键对应的Action
+
+ @param key 键字符串
+ @param modifiers 控制键类型
+ @param action 行为Action
+ */
 - (void)registerSimulatorShortcutWithKey:(NSString *)key
                                modifiers:(UIKeyModifierFlags)modifiers
                                   action:(KeyBoardAction)action;
